061c43522050f3bc9fa3d93206c6e69aa679eada
prashanth-reachvel <158998133+prashanth-reachvel@users.noreply.github.com>
2024-03-06 16:48:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -76,9 +76,10 @@ app.get("/api/requests", async (req, res) => {
         date: 1,
         selectedFile: 1,
         status: 1,
-      },
-      { limit: 5, sort: { date: -1 } } // Limit to latest 5 and sort by date descending
-    );
+      }
+    )
+      .sort({ date: -1 }) // Sort by date descending
+      .limit(10); // Limit to latest 5
     res.json(requests);
   } catch (err) {
     console.error("Error fetching requests:", err);
@@ -179,35 +180,65 @@ app.get("/api/inventory/:schoolName", async (req, res) => {
   }
 });
 
-// Endpoint to get inventory data by schoolName and title
+// app.get("/api/inventory/:school/:title", async (req, res) => {
+//   try {
+//     const { school, title } = req.params;
+//     const inventory = await Inventory.aggregate([
+//       {
+//         $match: {
+//           school: school,
+//           title: title,
+//         },
+//       },
+//       {
+//         $addFields: {
+//           latestDate: { $max: ["$createdDate", "$updatedDate"] },
+//         },
+//       },
+//       {
+//         $sort: {
+//           latestDate: -1,
+//         },
+//       },
+//       {
+//         $limit: 1,
+//       },
+//     ]);
+//     if (inventory.length === 0) {
+//       return res.status(404).json({ error: "Inventory not found" });
+//     }
+//     res.json(inventory[0]);
+//   } catch (error) {
+//     console.error("Error fetching inventory:", error);
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// });
+
 app.get("/api/inventory/:school/:title", async (req, res) => {
   try {
     const { school, title } = req.params;
-    const inventory = await Inventory.aggregate([
-      {
-        $match: {
-          school: school,
-          title: title,
-        },
-      },
-      {
-        $addFields: {
-          latestDate: { $max: ["$createdDate", "$updatedDate"] },
-        },
-      },
-      {
-        $sort: {
-          latestDate: -1,
-        },
-      },
-      {
-        $limit: 1,
-      },
-    ]);
-    if (inventory.length === 0) {
-      return res.status(404).json({ error: "Inventory not found" });
-    }
-    res.json(inventory[0]);
+    const inventory = await Inventory.find({ school: school });
+
+    // Get unique titles from the inventory
+    const uniqueTitles = [...new Set(inventory.map((item) => item.title))];
+
+    // Get the latest data for each unique title
+    const latestInventoryData = uniqueTitles.map((title) => {
+      const itemsWithTitle = inventory.filter((item) => item.title === title);
+      const latestItem = itemsWithTitle.reduce((prev, current) => {
+        const dateA = new Date(prev.updatedDate || prev.createdDate);
+        const dateB = new Date(current.updatedDate || current.createdDate);
+        return dateA > dateB ? prev : current;
+      });
+      return latestItem;
+    });
+
+    // Filter by title if provided
+    const filteredInventory = title
+      ? latestInventoryData.filter((item) => item.title === title)
+      : latestInventoryData;
+
+    res.json(filteredInventory);
   } catch (error) {
     console.error("Error fetching inventory:", error);
     res.status(500).json({ error: "Internal server error" });
@@ -327,39 +358,96 @@ app.put("/api/profile/updatePassword/:schoolName", async (req, res) => {
   }
 });
 
-// Endpoint to fetch the latest record for each school based on title
-app.get("/api/inventories", async (req, res) => {
+// Endpoint to update Student count
+app.put("/api/profile/updateStudents/:schoolName", async (req, res) => {
   try {
-    const latestInventories = await Inventory.aggregate([
-      {
-        $addFields: {
-          latestDate: { $max: ["$createdDate", "$updatedDate"] }, // Calculate the maximum date between createdDate and updatedDate
-        },
-      },
-      {
-        $sort: {
-          school: 1,
-          title: 1,
-          latestDate: -1, // Sort by the latestDate in descending order
-        },
-      },
-      {
-        $group: {
-          _id: "$school",
-          latestItem: { $first: "$$ROOT" }, // Get the latest item for each school
-        },
-      },
-      {
-        $replaceRoot: { newRoot: "$latestItem" }, // Replace the root with the latestItem
-      },
-    ]);
-    console.log(latestInventories);
-    res.json(latestInventories);
+    const { schoolName } = req.params;
+    const { students } = res.body;
+    const updatedSchool = await School.findOneAndUpdate(
+      { schoolName: schoolName },
+      { students: students },
+      { new: true }
+    );
+    res.json({ message: "Students count updated successfully", updatedSchool });
   } catch (error) {
-    console.error("Error fetching latest inventories:", error);
+    console.error("Error updating students count:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
+// app.get("/api/inventories", async (req, res) => {
+//   try {
+//     const allInventories = await Inventory.find({});
+//     res.json(allInventories);
+//   } catch (error) {
+//     console.error("Error fetching all inventories:", error);
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// });
+
+app.get("/api/inventories", async (req, res) => {
+  try {
+    const inventory = await Inventory.find({});
+
+    // Group inventory items by school
+    const inventoryBySchool = inventory.reduce((acc, item) => {
+      const currentItemDate = new Date(item.updatedDate || item.createdDate);
+      const currentLatestDate = acc[item.school]
+        ? new Date(acc[item.school].updatedDate || acc[item.school].createdDate)
+        : 0; // Initialize with 0
+
+      if (!acc[item.school] || currentItemDate > currentLatestDate) {
+        acc[item.school] = item;
+      }
+      return acc;
+    }, {});
+
+    // Convert the object back to an array
+    const latestInventoryData = Object.values(inventoryBySchool);
+
+    console.log(latestInventoryData);
+    res.json(latestInventoryData);
+  } catch (error) {
+    console.error("Error fetching inventory:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// Endpoint to fetch the latest record for each school based on title
+// app.get("/api/inventories", async (req, res) => {
+//   try {
+//     const latestInventories = await Inventory.aggregate([
+//       {
+//         $addFields: {
+//           updatedDate: {
+//             $cond: {
+//               if: { $eq: [{ $type: "$updatedDate" }, "date"] },
+//               then: "$updatedDate",
+//               else: { $toDate: "$updatedDate" },
+//             },
+//           },
+//         },
+//       },
+//       {
+//         $sort: { updatedDate: -1 },
+//       },
+//       {
+//         $group: {
+//           _id: "$school", // Group by school
+//           latestItem: { $first: "$$ROOT" }, // Get the first document (latest) in each group
+//         },
+//       },
+//       {
+//         $replaceRoot: { newRoot: "$latestItem" }, // Replace the root document with the latest inventory
+//       },
+//     ]);
+
+//     res.json(latestInventories);
+//   } catch (error) {
+//     console.error("Error fetching latest inventories:", error);
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// });
 
 // Endpoint to fetch all schools and their titles
 app.get("/api/schools", async (req, res) => {
@@ -406,7 +494,6 @@ app.put("/api/nextshipment/:schoolName/:title", async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 });
-
 app.listen(port, () => {
   console.log("Server is running on: " + port);
 });
